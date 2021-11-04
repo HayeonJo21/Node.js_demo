a4f026de8c19efeaf60d590d5d77e3aab46255ce
HayeonJo21 <gkdus6629@gmail.com>
2021-11-04 11:17:09 +0900

    main.js 필요없는 코드 삭제

--- main.js
+++ main.js
@@ -24,18 +24,6 @@ db.once("open", ()=>{
   console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
-
-// Subscriber.create(
-//   {
-//     name: "Esther Park",
-//     email: "esther@email.com"
-//   },
-//   function(error, saveDocument){
-//     if (error) console.log(error);
-//     console.log(saveDocument);
-//   );
-//   }
-
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -49,14 +37,10 @@ app.use(
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome to Confetti Cuisine!");
-// });
-
 app.get("/", homeController.showIndex);
 app.get("/courses", homeController.showCourses);
 app.get("/login", homeController.loginForm);
-app.get("/registerForm", homeController.registerForm);
+app.get("registerForm", homeController.registerForm);
 app.get("/gameSound", homeController.gameSoundMain);
 app.get("/market", homeController.showMarket);
 app.get("/jam", homeController.showJam);
