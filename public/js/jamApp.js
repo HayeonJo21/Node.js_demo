c5bc14b73df76717df1dea521bfbcab67cb75b32
HayeonJo21 <gkdus6629@gmail.com>
2021-11-17 09:54:21 +0900

    모달에 <hr> 추가

--- public/js/jamApp.js
+++ public/js/jamApp.js
@@ -4,8 +4,8 @@ $(document).ready(() => {
     $(".modal-body").html("");
     $.get("/jam/main?format=json", (data) => {
       data.forEach((jam) => {
-        $(".modal-body").append('<div><span class="jam-title">' +`${jam.title}`
-        + '</span><div class="jam-description">' + `${jam.description}` + '</div></div>' + '<hr>' 
+        $(".modal-body").append('<div><span class="jam-title"><strong>' +`${jam.title}`
+        + '</strong></span><div class="jam-description">' + `${jam.description}` + '</div></div>' + '<hr>' 
         );
       });
     });
